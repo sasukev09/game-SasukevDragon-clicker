2736871e50ab281516a1b0f77c7f5e82ef16d684
sasukev09 <ksala83@wgu.edu>
2024-03-13 14:19:45 -0500

    Simple Dragon Click Game V1

--- dragonFootballGame.py
+++ dragonFootballGame.py
@@ -3,6 +3,7 @@ import turtle
 
 # Creating the screen, setting the title and background color
 main_window = turtle.Screen()
+main_window.setup(900, 600)
 main_window.title("Dragon game")
 main_window.bgcolor("green")
 
@@ -14,11 +15,25 @@ sasukev = turtle.Turtle()
 sasukev.shape("sasukevDragonGIF.gif")
 sasukev.speed(1)
 
+clicks = 0
+pen = turtle.Turtle()
+pen.hideturtle()
+pen.color("white")
+pen.penup()
+pen.goto(0, 200)
+pen.write(f"Clicks: {clicks}", align="center", font=("Courier New", 32, "bold"))
 
 
+# Function that tracks clicks
 
 
+def clicked(x,y):
+    global clicks
+    clicks += 1
+    pen.clear()
+    pen.write(f"Clicks: {clicks}", align="center", font=("Courier New", 32, "bold"))
+
+sasukev.onclick(clicked)
 
 
 main_window.mainloop()
-
